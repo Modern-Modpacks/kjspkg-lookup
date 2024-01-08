4ffcd4e1fde4f5256c62cb14f4bd6590552d5b5a
Gcat101 <gcatmail2@gmail.com>
2024-01-08 15:38:18 +0300

    dev tests in prod, INSTANTLY REGRETS IT

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
+	import { page } from '$app/stores';
 	import { userPreferencesStore } from '$lib/stores';
 	import { onMount } from 'svelte';
 
