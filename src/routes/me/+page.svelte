9a17558e6670db03045f5a76fb9fb4167c312ead
Gcat101 <gcatmail2@gmail.com>
2024-01-30 16:42:18 +0300

    add /me (TODO)

---
+++ src/routes/me/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { base } from "$app/paths";
+	import { ghApiLoginStore } from "$lib/stores";
+
+    // TODO: add a page if login is not found
+    ghApiLoginStore.subscribe(login => {
+        goto(`${base}/s/?q=@author:${login}`)
+    })
+</script>
