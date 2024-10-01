d900bacfbf017402df29f5fb4d0e8da56039a26e
Gcat101 <gcatmail2@gmail.com>
2024-10-01 20:35:37 +0300

    Move away from /kjspkg-lookup as base

---
+++ src/routes/kjspkg-lookup/[...path]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		goto(base + "/" + $page.params.path + $page.url.searchParams + $page.url.hash);
+	});
+</script>
